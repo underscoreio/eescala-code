e7d31f34e1ef1eb6ce745d746b63682677892d04
Dave Gurnell <dave@untyped.com>
2014-11-26 17:51:34 +0000

    Rename findGt to findGreater

--- part4/src/main/scala/Part4.scala
+++ part4/src/main/scala/Part4.scala
@@ -5,31 +5,31 @@ object Part4 extends App {
   case object NotFound extends FindResult
 
   trait IntList {
-    def findGt(target: Int): FindResult = {
+    def findGreater(target: Int): FindResult = {
       this match {
         case Empty => NotFound
         case Cell(h, t) =>
           if(h >= target) {
             Found(h)
           } else {
-            t.findGt(target)
+            t.findGreater(target)
           }
       }
     }
 
-    def findGt2(target: Int): FindResult
+    def findGreater2(target: Int): FindResult
   }
 
   case object Empty extends IntList {
-    def findGt2(target: Int) = NotFound
+    def findGreater2(target: Int) = NotFound
   }
 
   case class Cell(head: Int, tail: IntList) extends IntList {
-     def findGt2(target: Int) = {
+     def findGreater2(target: Int) = {
       if(head >= target) {
         Found(head)
       } else {
-        tail.findGt(target)
+        tail.findGreater(target)
       }
     }
   }
@@ -37,7 +37,7 @@ object Part4 extends App {
   val a: IntList = Cell(1, Cell(2, Cell(3, Empty)))
   val b: IntList = Empty
 
-  println("The first number >= 2 in a is " + a.findGt(2))
-  println("The first number >= 2 in b is " + b.findGt(2))
+  println("The first number >= 2 in a is " + a.findGreater(2))
+  println("The first number >= 2 in b is " + b.findGreater(2))
 
 }
